96b30309bd3d640ef048076b465cb4f4b1b97c5e
GuilhermePrado001 <guilherme-prado-@outlook.com.br>
2020-11-13 17:35:08 -0300

    Phase 1 - Treatment for empty card

--- src/app/TopFive/TopFive.jsx
+++ src/app/TopFive/TopFive.jsx
@@ -1,5 +1,5 @@
 import '../TopFive/TopFive.scss'
-import { Card, Col, Row } from 'antd';
+import { Card, Col, Row, Empty } from 'antd';
 import InfoCard from '../../components/InfoCard/InfoCard';
 import { useContext } from 'react';
 import { ManagementTeamContext } from '../../context/ManagementTeamContext';
@@ -11,28 +11,39 @@ function TopFive() {
 
     return (
         <>
-          <Row justify="center">
-             <Col lg={{ span: 11, offset: 1 }} xs={{ span: 16 }}>  
-                <span className="avarage-title">Highest avg age</span>
-                <Card
-                    className="radius-modify avarage-card"
-                    bordered={true}>
-                    {ageAvg.map((e,i) => (
-                        <InfoCard key={i} name={e.name} avg={AvgArrayCalc(e.ageList)} />
-                    ))}
-                </Card>
-             </Col>   
-             <Col lg={{ span: 11, offset: 1 }} xs={{ span: 16 }}>
-                <span className="avarage-title">Lowest avg age</span>  
-                <Card
-                    className="radius-modify avarage-card"
-                    bordered={true}>
-                    {ageAvg.map((e,i) => (
-                        <InfoCard key={i} name={e.name} avg={AvgArrayCalc(e.ageList)} />
-                    ))}
-                </Card>
-             </Col>     
-          </Row>
+            <Row justify="center">
+                <Col lg={{ span: 11, offset: 1 }} xs={{ span: 16 }}>
+                    <span className="avarage-title">Highest avg age</span>
+                    {ageAvg.length > 0 ? (
+                        <Card
+                            className="radius-modify avarage-card"
+                            bordered={true}>
+                            {ageAvg.map((e, i) => (
+                                <InfoCard key={i} name={e.name} avg={AvgArrayCalc(e.ageList)} />
+                            ))}
+                        </Card>) :
+                        (<Card>
+                            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                        </Card>)
+                    }
+
+                </Col>
+                <Col lg={{ span: 11, offset: 1 }} xs={{ span: 16 }}>
+                    <span className="avarage-title">Lowest avg age</span>
+                    {ageAvg.length > 0 ? (
+                        <Card
+                            className="radius-modify avarage-card"
+                            bordered={true}>
+                            {ageAvg.map((e, i) => (
+                                <InfoCard key={i} name={e.name} avg={AvgArrayCalc(e.ageList)} />
+                            ))}
+                        </Card>) :
+                        (<Card>
+                            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                        </Card>)
+                    }
+                </Col>
+            </Row>
         </>
     );
 }
